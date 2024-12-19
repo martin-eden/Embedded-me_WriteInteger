95b2496cc12e63f6bb807ec6ad4cf26bc18f98ce
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-19 19:47:19 +0200

    [~] Sync with [me_Uart].

--- examples/me_CodecDecInt/me_CodecDecInt.ino
+++ examples/me_CodecDecInt/me_CodecDecInt.ino
@@ -10,12 +10,11 @@
 #include <me_BaseTypes.h>
 #include <me_Console.h>
 #include <me_Uart.h>
-#include <me_UartSpeeds.h>
 #include <me_MemorySegment.h>
 
 void setup()
 {
-  me_Uart::Init(me_UartSpeeds::Bps_115k);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
 
   Console.Print("[me_CodecDecInt] Okay, we are here.");
   RunTest();
