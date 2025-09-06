03a12c511844f0264f76591ef2b1dc4dbf346a86
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-09-06 21:30:00 +0200

    [~^] [Example]

--- examples/me_WriteInteger/me_WriteInteger.ino
+++ examples/me_WriteInteger/me_WriteInteger.ino
@@ -8,88 +8,76 @@
 #include <me_WriteInteger.h>
 
 #include <me_BaseTypes.h>
-#include <me_BaseInterfaces.h>
 #include <me_Console.h>
-#include <me_StreamsCollection.h>
 
 void Test_U1(
-  TUint_1 Value,
-  IOutputStream * OutputSream
+  TUint_1 Value
 )
 {
   Console.Write("TUint_1 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_U1(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void Test_U2(
-  TUint_2 Value,
-  IOutputStream * OutputSream
+  TUint_2 Value
 )
 {
   Console.Write("TUint_2 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_U2(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void Test_U4(
-  TUint_4 Value,
-  IOutputStream * OutputSream
+  TUint_4 Value
 )
 {
   Console.Write("TUint_4 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_U4(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void Test_S1(
-  TSint_1 Value,
-  IOutputStream * OutputSream
+  TSint_1 Value
 )
 {
   Console.Write("TSint_1 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_S1(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void Test_S2(
-  TSint_2 Value,
-  IOutputStream * OutputSream
+  TSint_2 Value
 )
 {
   Console.Write("TSint_2 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_S2(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void Test_S4(
-  TSint_4 Value,
-  IOutputStream * OutputSream
+  TSint_4 Value
 )
 {
   Console.Write("TSint_4 ");
-  me_WriteInteger::Encode(Value, OutputSream);
+  me_WriteInteger::Write_S4(Value, Console.GetOutputStream());
   Console.EndLine();
 }
 
 void RunTests()
 {
-  me_StreamsCollection::TUartOutputStream OutputSream;
+  Test_U1(TUint_1_Max);
+  Test_U2(TUint_2_Max);
+  Test_U4(TUint_4_Max);
 
-  OutputSream.Init();
+  Test_S1(TSint_1_Min);
+  Test_S1(TSint_1_Max);
 
-  Test_U1(TUint_1_Max, &OutputSream);
-  Test_U2(TUint_2_Max, &OutputSream);
-  Test_U4(TUint_4_Max, &OutputSream);
+  Test_S2(TSint_2_Min);
+  Test_S2(TSint_2_Max);
 
-  Test_S1(TSint_1_Min, &OutputSream);
-  Test_S1(TSint_1_Max, &OutputSream);
-
-  Test_S2(TSint_2_Min, &OutputSream);
-  Test_S2(TSint_2_Max, &OutputSream);
-
-  Test_S4(TSint_4_Min, &OutputSream);
-  Test_S4(TSint_4_Max, &OutputSream);
+  Test_S4(TSint_4_Min);
+  Test_S4(TSint_4_Max);
 }
 
 void setup()
